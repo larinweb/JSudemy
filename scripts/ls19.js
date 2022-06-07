9314ca63d0fe5da2278d7a6e02e2d1101743fab5
Slava <example@gmail.com>
2022-06-07 18:25:02 +0300

    Done by myself

--- scripts/ls19.js
+++ scripts/ls19.js
@@ -1,1 +1,29 @@
 "use strict";
+// 1)
+const numberOfFilms = +prompt("Сколько фильмов вы уже посмотрели?", "");
+
+// 2)
+const personalMovieDB = {
+    count: numberOfFilms,
+    movies: {},
+    actors: {},
+    genres: {},
+    privat: false
+};
+
+//console.log(personalMovieDB['count']);
+
+// 3)
+const movies = {
+    movie1: {
+        movieLast: prompt("Один из последних просмотренных фильмов?", ""),
+        movieScore: +prompt("На сколько оцените его?", "")
+    },
+    movie2: {
+        movieLast: prompt("Один из последних просмотренных фильмов?", ""),
+        movieScore: +prompt("На сколько оцените его?", "")
+    }
+};
+
+//console.log(movies);
+//console.log(movieLast,movieScore);
