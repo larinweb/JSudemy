5853ac9377b04a878e2128a4ac8e7ba218d2f62b
Slava <example@gmail.com>
2022-06-08 15:30:24 +0300

    Finish 20 lesson

--- scripts/ls20.js
+++ scripts/ls20.js
@@ -1,2 +1,38 @@
 "use strict";
 
+if (4 == 9) {
+  console.log('Ok');
+} else {
+  console.log('Error');
+}
+
+const num = 50;
+
+if (num < 49) {
+  console.log('Error');
+} else if (num > 100) {
+  console.log('Too many');
+} else {
+  console.log('Ok');
+}
+
+// Тернарный оператор (три аргумента a ? b : c)
+const num2 = 50;
+(num2 === 50) ? console.log('Ok!'): console.log('Error');
+
+
+const num3 = 50;
+switch (num3) {
+  case 49:
+    console.log('Incorect');
+    break;
+  case 100:
+    console.log('Incorect');
+    break;
+  case 50:
+    console.log('Ok');
+    break;
+  default:
+    console.log('Not this time');
+    break;
+}
