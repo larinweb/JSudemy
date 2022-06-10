0cceb29eeca6cba47e7c3cdd73a0294ea24534b3
Slava <example@gmail.com>
2022-06-10 14:53:38 +0300

    after  homework 3 incorrect answer

--- scripts/ls21.js
+++ scripts/ls21.js
@@ -42,3 +42,50 @@ let johnReport, alexReports, samReport, mariReport = 'done';
 console.log((johnReport || johnReport || samReport || mariReport));
 
 console.log(!0); // оператор "НЕ"
+
+
+// ------------ Tasks
+ 
+//Что выведет в консоль код?
+
+console.log( NaN || 2 || undefined ); //t 2
+console.log( NaN && 2 && undefined ); //f nan
+console.log( 1 && 2 && 3 ); //t 3
+console.log( !1 && 2 || !3 ); //f
+console.log( 25 || null && !3 ); //t
+console.log( NaN || null || !3 || undefined || 5); //t 5
+console.log( NaN || null && !3 && undefined || 5); //t 5
+console.log( 5 === 5 && 3 > 1 || 5); //t
+
+//Выполняется ли условие?
+
+const hamburger = 3;
+const fries = 3;
+const cola = 0;
+const nuggets = 2;
+ 
+if (hamburger === 3 && cola || fries === 3 && nuggets) {
+   console.log('Done!');
+}
+// Done.
+
+let hamburger;
+const fries = NaN;
+const cola = 0;
+const nuggets = 2;
+ 
+if (hamburger || cola || fries === 3 || nuggets) {
+   console.log('Done!');
+}
+// Done
+
+let hamburger;
+const fries = NaN;
+const cola = 0;
+const nuggets = 2;
+ 
+if (hamburger && cola || fries === 3 && nuggets) {
+   console.log('Done!');
+}
+// none
+
