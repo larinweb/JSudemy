2aabdf66914ac3e2c7bd66fd5375face9e4f33ec
Slava <example@gmail.com>
2022-06-10 18:03:58 +0300

    Lesson 22 finish

--- scripts/ls22.js
+++ scripts/ls22.js
@@ -1,1 +1,30 @@
 "use strict";
+
+let num = 50;
+
+// while (num <= 55) {
+//   console.log(num);
+//   num++;
+// }
+
+do {
+  console.log(num);
+  num++;
+}
+while (num < 55);
+
+// let num2 = 50;
+
+// for (let i = 1; i < 8; i++){
+//   console.log(num2);
+//   num2++;
+// }
+
+for (let i = 1; i < 10; i++) {
+  if (i === 10) {
+    // break;
+    continue;
+  }
+
+  console.log(i);
+}
