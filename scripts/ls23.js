ba97770dc2f4d8f5d03ff09d123737bcc2522b0a
Slava <example@gmail.com>
2022-06-13 14:16:11 +0300

    Additional practice tree and reverse tree

--- scripts/ls23.js
+++ scripts/ls23.js
@@ -29,6 +29,33 @@ for (let i = 1; i < lenght; i++) {
 console.log(result);
 
 
+let treeResult = '';
+const treeLenght = 7;
+
+for (let treeLine = 1; treeLine < treeLenght; treeLine++) {
+
+    for (let treeColumn = 0; treeColumn < treeLine; treeColumn++) {
+        treeResult += '*';
+    }
+
+    treeResult += '\n'; // perenos stroki
+}
+
+console.log(treeResult);
+
+
+let reverse = '';
+const reverseHeight = '8';
+
+for (let reverseLine = 0; reverseLine < reverseHeight; reverseLine++) {
+    for (let reverseColumn = reverseHeight; reverseColumn > reverseLine; reverseColumn--) {
+        reverse += '*';
+    }
+    reverse += '\n'; // perenos stroki
+}
+console.log(reverse);
+
+
 
 first: for (let i = 0; i < 3; i++) {
     console.log(`First level: ${i}`);
