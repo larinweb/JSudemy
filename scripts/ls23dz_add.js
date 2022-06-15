29c690dbc6a5a630c0e77d00e5ed000a1bf0faaa
Slava <example@gmail.com>
2022-06-15 15:52:35 +0300

    symetric Tree

--- scripts/ls23dz_add.js
+++ scripts/ls23dz_add.js
@@ -1,27 +1,46 @@
 "use strict";
-const arr = [3, 5, 8, 16, 20, 23, 50];
-const result = [];
-for (let i = 0; i < arr.length; i++) {
-  result[i] = arr[i];
-}
+// const arr = [3, 5, 8, 16, 20, 23, 50];
+// const result = [];
+// for (let i = 0; i < arr.length; i++) {
+//   result[i] = arr[i];
+// }
 
-console.log(result);
+// console.log(result);
 
-const data = [5, 10, 'Shopping', 20, 'Homework'];
-for (let i = 0; i < data.length; i++) {
-  if (typeof (data[i]) === "number") {
-    data[i] = data[i] * 2;
-  } else if (typeof (data[i]) === "string") {
-    data[i] = `${data[i]} - done`;
+// const data = [5, 10, 'Shopping', 20, 'Homework'];
+// for (let i = 0; i < data.length; i++) {
+//   if (typeof (data[i]) === "number") {
+//     data[i] = data[i] * 2;
+//   } else if (typeof (data[i]) === "string") {
+//     data[i] = `${data[i]} - done`;
+//   }
+// }
+
+// console.log(data);
+
+// const data = [5, 10, 'Shopping', 20, 'Homework'];
+// const result = [];
+// for (let i = 1; i <= data.length; i++) {
+//   result[i - 1] = data[data.length - i]
+// }
+
+// console.log(result);
+
+
+const lines = 5;
+let result = '';
+
+for (let i = 0; i <= lines; i++) {
+
+  for (let j = 0; j < lines - i; j++) {
+    result += ' ';
   }
-}
 
-console.log(data);
+  for (let j = 0; j < 2 * i + 1; j++) {
+    result += "*";
+  }
 
-const data = [5, 10, 'Shopping', 20, 'Homework'];
-const result = [];
-for (let i = 1; i <= data.length; i++) {
-  result[i - 1] = data[data.length - i]
+  result += '\n';
 }
 
 console.log(result);
